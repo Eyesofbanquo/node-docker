5601ff0e01cb3bea1a8f9de9a8c2ec8df5e51d2e
Markim Shaw <ms79723@gmail.com>
2020-11-21 23:00:51 -0600

    install pg

--- server.ts
+++ server.ts
@@ -8,6 +8,10 @@ app.get("/", (request, response) => {
   response.send({ success: true });
 });
 
+app.get("/hello", (request, response) => {
+  response.send({ data: "Hello world" });
+});
+
 app.listen(3000, () => {
   console.log("Running on 3000");
 });
