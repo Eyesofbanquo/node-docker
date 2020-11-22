80204d7b69494b155414439a395b5083fde03a4c
Markim Shaw <ms79723@gmail.com>
2020-11-22 17:02:49 -0600

    Add uuid extension

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -15,16 +15,21 @@ const createDefaultDatabase = async () => {
   await pool.query(`CREATE DATABASE avon`).catch();
 };
 
+const createUUIDExtension = async () => {
+  await pool.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`).catch();
+};
 const createDefaultTable = async () => {
   await pool.query(`CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
     username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    created_date TIMESTAMP DEFAULT (now() at time zone 'utc')
   )`);
 };
 
-export const setup = () => {
-  createDefaultTable();
+export const setup = async () => {
+  await createUUIDExtension();
+  await createDefaultTable();
 };
 
 console.log("Created Database!");
