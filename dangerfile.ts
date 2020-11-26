61cc5d3b8ab3865a22db101213da532ed01d8988
Markim Shaw <ms79723@gmail.com>
2020-11-25 20:24:14 -0600

    update danger

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,4 @@
-import { message, danger } from "danger";
+import { message, danger, warn } from "danger";
 
 const pr_title = danger.github.pr.title;
 message("Looking at " + pr_title);
@@ -6,5 +6,28 @@ message("Looking at " + pr_title);
 const modifiedMD = danger.git.modified_files.join("- ");
 message("Changed Files in this PR: \n - " + modifiedMD);
 
-const tests = danger.git.fileMatch("./**/*.test.ts");
-message("All of the test files: \n " + tests);
+const commits = danger.git.commits;
+const commitsWithoutMessages = commits.filter(
+  (commit) => commit.message.length === 0
+);
+
+if (commitsWithoutMessages.length > 0) {
+  warn(
+    "These commits should've had messages:\n" +
+      commitsWithoutMessages.join("\n")
+  );
+}
+
+const routes = danger.git.modified_files.filter((modifiedFile) =>
+  modifiedFile.includes("route")
+);
+
+const routesWithoutTests = routes.filter(
+  (route) => danger.git.modified_files.includes(`${route}.test.ts`) === false
+);
+
+if (routesWithoutTests.length > 0 && routes.length > 0) {
+  message(
+    "The following routes need tests: \n" + routesWithoutTests.join("\n")
+  );
+}
