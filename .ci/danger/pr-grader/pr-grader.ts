63e81f2e7baeb0013936f1ab8538160f6e24470f
Markim Shaw <ms79723@gmail.com>
2020-12-02 21:22:49 -0600

    remove extra amount

--- .ci/danger/pr-grader/pr-grader.ts
+++ .ci/danger/pr-grader/pr-grader.ts
@@ -1,31 +1,31 @@
-import { message, danger, warn, fail, markdown } from "danger";
+// import { message, danger, warn, fail, markdown } from "danger";
 
-const commits = danger.github.commits;
+// const commits = danger.github.commits;
 
-const setupCommits = commits.find((commit) =>
-  commit.commit.message.includes("🔓")
-);
-const actionCommits = commits.find((commit) =>
-  commit.commit.message.includes("🎬")
-);
-const testCommits = commits.find((commit) =>
-  commit.commit.message.includes("🔒")
-);
+// const setupCommits = commits.find((commit) =>
+//   commit.commit.message.includes("🔓")
+// );
+// const actionCommits = commits.find((commit) =>
+//   commit.commit.message.includes("🎬")
+// );
+// const testCommits = commits.find((commit) =>
+//   commit.commit.message.includes("🔒")
+// );
 
-export const grader = () => {
-  if (setupCommits) {
-    warn(
-      "Missing a commit indicating the setup for this PR. Please include commit with 🔓 in the title next time."
-    );
-  }
-  if (actionCommits) {
-    warn(
-      "Missing a commit indicating the action for this PR. Please include commit with 🎬 in the title next time."
-    );
-  }
-  if (testCommits) {
-    warn(
-      "Missing a commit indicating the test for this PR. Please include commit with 🔒 in the title next time."
-    );
-  }
-};
+// export const grader = () => {
+//   if (setupCommits) {
+//     warn(
+//       "Missing a commit indicating the setup for this PR. Please include commit with 🔓 in the title next time."
+//     );
+//   }
+//   if (actionCommits) {
+//     warn(
+//       "Missing a commit indicating the action for this PR. Please include commit with 🎬 in the title next time."
+//     );
+//   }
+//   if (testCommits) {
+//     warn(
+//       "Missing a commit indicating the test for this PR. Please include commit with 🔒 in the title next time."
+//     );
+//   }
+// };
